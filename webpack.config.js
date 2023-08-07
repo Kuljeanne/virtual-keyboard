fcb0f530dc6558439fb1e363336913d627ff2226
Zhanna Kulaginae <kuljeanne@gmail.com>
2023-08-07 20:13:16 +0300

    fix: eslintignore

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
