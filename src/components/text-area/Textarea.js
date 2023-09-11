29c958996c98f2bfdf1ac1afd3cc5c399c74b8f9
Zhanna Kulagina <kuljeanne@gmail.com>
2023-09-11 16:03:21 +0300

    fix prod

--- src/components/text-area/Textarea.js
+++
@@ -1,7 +0,0 @@
-import { createNode } from '../../utils/utils';
-
-export default function TextArea() {
-  const node = createNode('textarea', 'textarea');
-  node.setAttribute('autofocus', true);
-  return node;
-}
